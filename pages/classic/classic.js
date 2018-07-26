46e4ac13ce0e041d814e633077f27d3eb680af42
zhaolingxue <18222478537@163.com>
2018-07-26 16:12:35 +0800

    获取数据

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -12,7 +12,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.request({
+      url:'http://bl.7yue.pro/v1/classic/latest',
+      header: {
+        appkey:"8dkZ01Sgxs4hIflV"
+      }
+    })
   },
 
   /**
